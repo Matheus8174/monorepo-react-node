1adf6794982f157804451c4f90c97a2a93a682e3
Matheus8174 <souzamatheus8174@gmail.com>
2022-01-01 12:45:58 -0300

    build: create webpack config file

---
+++ packages/frontend/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  mode: isDevelopment ? 'development' : 'production',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  devServer: {
+    static: {
+      serveIndex: true,
+      watch: true,
+    },
+    historyApiFallback: true,
+    hot: true,
+    host: 'localhost',
+    port: 4000
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    }),
+    isDevelopment && new ReactRefreshWebpackPlugin()
+  ].filter(Boolean)
+};
