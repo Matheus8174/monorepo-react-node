45a7e914cc4b4c6be825b759ab947ba982f3bdab
Matheus Araújo <76668582+Matheus8174@users.noreply.github.com>
2022-01-07 12:46:18 -0300

    build: update babel config add plugin transform-runtime

--- packages/frontend/babel.config.js
+++ packages/frontend/babel.config.js
@@ -3,5 +3,6 @@ module.exports = {
     '@babel/preset-env',
     ['@babel/preset-react', { 'runtime': 'automatic' }],
     '@babel/preset-typescript'
-  ]
+  ],
+  plugins: [['@babel/transform-runtime', { 'regenerator': true }]]
 };
